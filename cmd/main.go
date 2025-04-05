bdc5d46908f1f07d74e2f81bdd4db6ab10ecf08f
Aditya-Byte <adityabyte@gmail.com>
2025-04-05 16:50:25 +0530

    Implemented a Command line interface for uploading and downloading file.

--- cmd/main.go
+++ cmd/main.go
@@ -1,60 +1,71 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
-	"strings"
+	"log"
 
 	"github.com/AdityaByte/bytemesh/client"
 	"github.com/AdityaByte/bytemesh/coordinator"
 	"github.com/AdityaByte/bytemesh/middleware"
 )
 
-// Program flow
-// main.go - entry point client communicate with it
-// then the request is being sent to the client's upload function
-// which uploads the file in a local storage location
-// After that the middleware creates the parts like how many parts are being created of it
-// and the chunk then everything goes correctly it returns a chunk array to me
-// which i then passed to the server function which takes care of the distribution of the chunks
-// to the datanodes right now we have 3 datanodes
-// if everything goes correctly then a chunk data nodes are stored at data nodes server correctly.
+// The main purpose of the distributed file storage system is to allow users to
+// upload files to the cloud and retrieve them when needed.
+//
+// One of the key advantages of a distributed file storage system is its fault tolerance,
+// which is achieved through its architecture.
+//
+// 1. **NameNode**: Responsible for handling metadata, such as file locations and storage management.
+// 2. **DataNode**: We currently have three DataNodes where file chunks are stored.
+// 3. **Client**: Can perform two main operations—uploading files or downloading them.
+// 4. **Middleware**: Manages file uploads, downloads, and metadata retrieval.
+// 5. **Coordinator**: Sends requests to servers, fetches data, and passes it to the middleware for further processing.
+
+// Initializing function which initialize all the environment variables by
+// loading out the .env file.
 
 func main() {
-	if len(os.Args) < 1 {
-		fmt.Println("Usage go run . <filelocation>")
-		return
+
+	const version = "v1.0.0"
+	const author = "@AdityaByte"
+	fileLocation := flag.String("u", "", "Upload File Location")
+	downloadFileName := flag.String("d", "", "Download File name")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage : %s\n", "bytemesh -u <Upload File Location>  -d <Download file name>")
+		fmt.Fprintf(flag.CommandLine.Output(), "Version : %s\n", version)
+		fmt.Fprintf(flag.CommandLine.Output(), "Author : %s\n", author)
+		fmt.Fprintf(flag.CommandLine.Output(), "Options:\n")
+		flag.PrintDefaults()
 	}
 
-	fileLocation := os.Args[1]
+	flag.Parse()
 
-	file, err := client.Upload(fileLocation)
+	if *fileLocation != "" {
+		file, err := client.Upload(*fileLocation)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
 
-	if err != nil {
-		fmt.Println(err)
-		return
+		chunks, filename, filesize, err := middleware.CreateChunk(file)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+
+		for i, chunk := range *chunks {
+			log.Println("Iteration:", i, "Chunk ID:", chunk.Id, "Data Length:", len(chunk.Data))
+		}
+
+		if err := coordinator.SendChunks(chunks, filename, filesize); err != nil {
+			log.Fatalf("%v", err)
+		}
 	}
 
-	chunks, filename, err := middleware.CreateChunk(file)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if err := coordinator.SendChunks(chunks, filename); err != nil {
-		fmt.Println("Error sending chunks to the server nodes", err)
-		return
-	}
-
-	for _, chunk := range *chunks {
-		fmt.Println("chunk id:", chunk.Id)
-		fmt.Println("chunk datasize:", len(chunk.Data), "byte")
-		fmt.Println("------------")
-	}
-
-	if err := client.Download(strings.TrimPrefix(filename, "storage/")); err != nil {
-		fmt.Println(err)
-		return
+	if *downloadFileName != "" {
+		if err := client.Download(*downloadFileName); err != nil {
+			log.Fatalf("%v", err)
+		}
 	}
 
 }
