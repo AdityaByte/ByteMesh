1ca80d97f6696fdfa167ad19012b4b2e4dfeab49
Aditya-Byte <adityabyte@gmail.com>
2025-04-05 16:45:18 +0530

    added a field actualdata which stores the actual file size in kb.

--- models/metadata.go
+++ models/metadata.go
@@ -3,5 +3,6 @@ package models
 type MetaData struct {
 	Filename string
 	FileExtension string
+	ActualSize float64
 	Location map[string]string
 }
--- namenode/model/metadata.go
+++ namenode/model/metadata.go
@@ -3,5 +3,6 @@ package model
 type MetaData struct {
 	Filename      string            `bson:"filename"`
 	FileExtension string            `bson: "fileExtension"`
+	ActualSize    float64           `bson:"actualFileSize"`
 	Location      map[string]string `bson: "location"`
 }
