dedf16a235e05f19d3a05d80a8b54403fbe36c55
Aditya-Byte <adityapawar4914@gmail.com>
2025-05-19 21:58:09 +0530

    Major: Implemented the login and signup functionality.

--- webclient/src/js/script.js
+++ webclient/src/js/script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     event.preventDefault()
     document.getElementById("get-started").addEventListener("click", function (event) {
         event.preventDefault()
-        window.location.href = "/src/auth.html"
+        window.location.href = "/webclient/src/auth.html"
     })
 })
