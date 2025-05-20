d1dc7c58925634fd2535ed2d9e5237a6443301f0
Aditya-Byte <adityapawar4914@gmail.com>
2025-05-21 00:08:55 +0530

    Implemented the dashboard for the webclient and also improved some UI.

--- webclient/src/js/auth.js
+++ webclient/src/js/auth.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(event){
+document.addEventListener('DOMContentLoaded', function (event) {
     event.preventDefault()
     // Here we have to handle authentication sending the request and fetching response all these things.
 
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(event){
     const loginForm = document.querySelector("#login-form")
     const signupForm = document.querySelector("#signup-form")
 
-    loginForm.addEventListener("submit", function(event){
+    loginForm.addEventListener("submit", function (event) {
         event.preventDefault()
         // let formData = new FormData(this) // Gives the form data of the current context.
         // Instead of sending the formdata directly we are sending the data via json.
@@ -43,24 +43,32 @@ document.addEventListener('DOMContentLoaded', function(event){
                     "content-type": "application/json",
                 }
             })
-            .then((response)=> {
-                if (response.status === 200) {
-                    window.location.href = "/webclient/src/dashboard.html"
-                } else {
-                    alert(response.status)
-                    console.log(response)
-                    console.log(response.text)
-                }
-            })
-            .catch((error)=> {
-                console.error(error)
-            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        localStorage.removeItem("authView")
+                        // So when the user logs in we need to save the token in the localstorage
+                        let token = response.headers.get("Authorization")
+                        if (token) {
+                            let trimmedToken = token.replace("Bearer ", "");
+                            localStorage.setItem("token", trimmedToken)
+                            window.location.href = "/webclient/src/dashboard.html"
+                        } else {
+                            throw new Error("Authorization header missing!")
+                        }
+                    }
+                    throw new Error(`ERROR: Response status ${response.status} and Response text ${response.text}`)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
         })() // IIFE
     })
 
 
-    signupForm.addEventListener("submit", function(event){
+    signupForm.addEventListener("submit", function (event) {
         event.preventDefault()
+
+        console.log("working..")
 
         const username = document.querySelector("[name=username]").value.trim()
         const password = document.querySelector("[name=password]").value.trim()
@@ -96,16 +104,20 @@ document.addEventListener('DOMContentLoaded', function(event){
             },
             body: JSON.stringify(payload)
         })
-        .then(response => {
-            if (response == 201) {
-                window.location.href = "/webclient/src/dashboard.html"
-                return
-            }
-            console.log(response.status)
-        })
-        .catch(error => {
-            console.error(error)
-        })
+            .then(response => {
+                if (response == 201) {
+                    alert("Credentials created now login")
+                    document.querySelector(".signup-content").classList.remove("active")
+                    document.querySelector(".login-content").classList.add("active")
+                    // window.location.href = "/webclient/src/dashboard.html"
+                    return
+                }
+                // Else we need to generate some error and print back the error
+                throw new Error(response.text)
+            })
+            .catch(error => {
+                console.error(error)
+            })
     })
 })
 
