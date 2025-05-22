96e6c5caf2987a29cc60cce365239f063dcb0cfd
Aditya-Byte <adityabyte@gmail.com>
2025-05-22 22:57:14 +0530

    Added some logs and improve the way to return error.

--- auth/service/signup.go
+++ auth/service/signup.go
@@ -8,14 +8,15 @@ import (
 	"github.com/AdityaByte/bytemesh/auth/config"
 	"github.com/AdityaByte/bytemesh/auth/model"
 	"github.com/AdityaByte/bytemesh/auth/repository"
+	"github.com/AdityaByte/bytemesh/datanodes/server1/logger"
 )
 
 func Signup(w http.ResponseWriter, r *http.Request) {
 
+	logger.InfoLogger.Println("Sign up request..")
 	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "Invalid Request Type")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -27,24 +28,23 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	// Here we have to Load the Mongo Repository
 	repo, err := config.LoadMongoRepository()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if err := repository.CheckUserExists(user.Username, repo); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, err.Error())
+		logger.ErrorLogger.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// Else the username is unique so we have to save it.
 	if err := repository.CreateUser(&user, repo); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	logger.InfoLogger.Println("Sign up successfull")
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "Signup successful")
 }
