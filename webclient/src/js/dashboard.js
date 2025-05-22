c5567410a9b274e17deece68f5d9ee9d9240d1e1
Aditya-Byte <adityapawar4914@gmail.com>
2025-05-22 22:58:07 +0530

    Bug fix: Optimized some UI error and make the code clean

--- webclient/src/js/dashboard.js
+++ webclient/src/js/dashboard.js
@@ -1,19 +1,10 @@
-// When the document is loaded initailly i need to set the content ok.
+const homeSwitchingBtn = document.getElementById("home-switcher")
+const uploadSwitchingBtn = document.getElementById("upload-switcher")
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Here we need to fetch out the content from the backend server.
-    // So here what i have to do i have to do some thing like search for the token thing n all.
 
-    const homeSwitchingBtn = document.getElementById("home-switcher")
-    const uploadSwitchingBtn = document.getElementById("upload-switcher")
     const logout = document.querySelector("#logout-btn")
-
-    logout.addEventListener("click", function (event) {
-        event.preventDefault()
-        // Here we need to clear the localstorage
-        localStorage.removeItem("user")
-        localStorage.removeItem("token")
-        window.location.href = "/webclient/src/index.html"
-    })
+    logout.addEventListener("click", logoutHandler)
 
     content = localStorage.getItem("content")
     if (content === "main-content") {
@@ -24,28 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".upload-content").style.display = "flex"
     }
 
-
-    homeSwitchingBtn.addEventListener("click", function (event) {
-        event.preventDefault()
-        // Now we need to make the other content hidden and show up the main one and have to do other task too like making the backend api request and show up the content.
-        document.querySelector(".main-content").style.display = "flex"
-        document.querySelector(".upload-content").style.display = "none"
-        homeSwitchingBtn.classList.add("active")
-        uploadSwitchingBtn.classList.remove("active")
-        localStorage.setItem("content", "main-content")
-    })
-
-    uploadSwitchingBtn.addEventListener("click", function (event) {
-        event.preventDefault()
-        document.querySelector(".main-content").style.display = "none"
-        document.querySelector(".upload-content").style.display = "flex"
-        uploadSwitchingBtn.classList.add("active")
-        homeSwitchingBtn.classList.remove("active")
-        localStorage.setItem("content", "upload-content")
-    })
-
+    homeSwitchingBtn.addEventListener("click", switchToHome)
+    uploadSwitchingBtn.addEventListener("click", switchToUpload)
 
     token = localStorage.getItem("token")
+
     if (!token || token.trim() === "") {
         // In that case alert that u cannot access the pages ok.
         alert("Unauthorized")
@@ -53,7 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
         return
     }
 
-    // console.log("token:", token)
     console.log("Authorized")
     // Else you can load the content.
     user = localStorage.getItem("user")
@@ -70,14 +43,35 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch(`${gatewayURL}/fetchall?user=${user}`, {
         method: "GET",
     })
-        .then(response => {
+        .then(async response => {
             if (response.status != 200) {
                 throw new Error("Failed to fetch files")
             }
             return response.json()
         })
         .then(jsonResponse => {
-            // Here we need to handle the 201 response.
+
+            const fileList = document.querySelector(".file-list")
+
+            // Checking the response is null.
+            if (jsonResponse === null) {
+                const element = document.createElement("h1")
+                element.textContent = "No file found :("
+                element.style.textAlign = "center"
+                fileList.style.justifyContent = "center"
+                fileList.style.alignItems = "center"
+                fileList.appendChild(element)
+                return
+            }
+            // If some files are found we need to remove it for proper managing the file-card.
+            fileList.style.justifyContent = "flex-start"
+            fileList.style.alignItems = "flex-start"
+            const h1 = fileList.querySelector("h1")
+            if (h1) {
+                fileList.removeChild(h1)
+            }
+
+            // Here we need to handle the 200 response.
             // <div class="file-card">
             // <img width="12px" alt="">
             // <span id="filename"></span>
@@ -86,14 +80,10 @@ document.addEventListener("DOMContentLoaded", function () {
             // </div>
 
             // Create a card something like that ok.
-            const fileList = document.querySelector(".file-list")
             const imgUrl = "https://imgs.search.brave.com/Ses02BbgloQAO7UoRZnwh1kPMpy0f_WYYJz8polU10Y/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtMDAuaWNvbmR1/Y2suY29tL2Fzc2V0/cy4wMC9pbWFnZS1m/aWxlLWljb24tNzgw/eDEwMjQtZnVjOXY1/cmwucG5n"
 
 
             // Now i have to set the content
-
-            console.log(jsonResponse)
-            console.log(typeof jsonResponse)
 
             jsonResponse.forEach(data => {
                 // here we have to create a structure ok.
@@ -101,7 +91,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 fileCard.className = "file-card"
                 const img = document.createElement("img")
                 img.src = `${imgUrl}`
-                img.width = "12px"
+                img.style.width = "12px"
                 const filename = document.createElement("span")
                 filename.className = "filename"
                 filename.textContent = data.Filename
@@ -110,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 uploadDate.textContent = data.UploadDate
                 const size = document.createElement("span")
                 size.className = "filesize"
-                size.textContent = data.Size
+                size.textContent = Math.round(data.Size) + "kb"
                 const downloadBtn = document.createElement("button")
                 downloadBtn.className = "download-btn"
                 downloadBtn.innerHTML = "Download"
@@ -136,29 +126,29 @@ document.addEventListener("DOMContentLoaded", function () {
         const response = await fetch(`${gatewayURL}/download?user=${user}&filename=${filename}`, {
             method: "GET"
         })
-        .then( async response => {
-            if (!response.ok) {
-                const errorText = await response.text()
-                throw new Error(errorText)
-            }
-            return response
-        })
-        .then(async response => {
-             // Since if the response is ok we need to extract the binary which was sent by the backend server.
-             const blob = await response.blob()
-             const url = window.URL.createObjectURL(blob)
-             const a = document.createElement("a")
-             a.href = url
-             a.download = filename
-             document.body.appendChild(a)
-             a.click()
-             a.remove()
-             window.URL.revokeObjectURL(url)
-        })
-        .catch(err => {
-            console.error("Download error:", err.message)
-            alert("Download failed:"+err.message)
-        })
+            .then(async response => {
+                if (!response.ok) {
+                    const errorText = await response.text()
+                    throw new Error(errorText)
+                }
+                return response
+            })
+            .then(async response => {
+                // Since if the response is ok we need to extract the binary which was sent by the backend server.
+                const blob = await response.blob()
+                const url = window.URL.createObjectURL(blob)
+                const a = document.createElement("a")
+                a.href = url
+                a.download = filename
+                document.body.appendChild(a)
+                a.click()
+                a.remove()
+                window.URL.revokeObjectURL(url)
+            })
+            .catch(err => {
+                console.error("Download error:", err.message)
+                alert("Download failed:" + err.message)
+            })
     }
 
 
@@ -166,7 +156,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const uploadForm = document.querySelector("#upload-form")
     const uploadInfo = document.querySelector("#upload-info")
 
-    uploadForm.addEventListener("submit", async function(event) {
+    uploadForm.addEventListener("submit", async function (event) {
         event.preventDefault()
         // Here what we need to do we need to fetch the file.
         const file = document.querySelector("input[name=file]").files[0]
@@ -187,26 +177,71 @@ document.addEventListener("DOMContentLoaded", function () {
             method: "POST",
             body: formData,
         })
-        .then(async response => {
-            return response.text().then(text => {
+            .then(async response => {
+                const responseText = await response.text()
+
                 if (response.status != 201) {
-                    throw new Error(text)
+                    throw new Error(responseText)
+                    return
                 }
-                return text
+                return responseText
             })
-        })
-        .then(async response => {
-            uploadInfo.textContent = response.text()
-            uploadInfo.style.color = "white"
-            uploadInfo.style.visibility = "visible"
-        })
-        .catch(err => {
-            console.error(err)
-            uploadInfo.textContent = "Upload failed: " + err.message;
-            uploadInfo.style.color = "red";
-            uploadInfo.style.visibility = "visible";
-        })
+            .then(async response => {
+                uploadInfo.textContent = response.message
+                uploadInfo.style.color = "green"
+                uploadInfo.style.visibility = "visible"
+            })
+            .catch(err => {
+                console.error(err)
+                uploadInfo.textContent = "Upload failed: " + err.message;
+                uploadInfo.style.color = "red";
+                uploadInfo.style.visibility = "visible";
+            })
+    })
+
+    // Handling the name of the file to the placeholder
+    const fileInput = document.querySelector("#dropzone-file")
+    const fileNameDisplay = document.querySelector("#file-name")
+    const dropzoneContent = document.querySelector(".dropzone-content")
+
+    fileInput.addEventListener("change", function() {
+        const file = this.files[0]
+        if (file) {
+            dropzoneContent.style.display = "none"
+            fileNameDisplay.textContent = `Selected file: ${file.name}`
+        } else {
+            fileNameDisplay.textContent = ""
+            dropzoneContent.style.display = "flex"
+        }
     })
 })
 
+// Logout handler
+const logoutHandler = async (event) => {
+    event.preventDefault()
+    localStorage.removeItem("user")
+    localStorage.removeItem("token")
+    localStorage.removeItem("content")
+    window.location.href = "/webclient/src/index.html"
+}
 
+// Switching to Home
+const switchToHome = async (event) => {
+    event.preventDefault()
+    // Now we need to make the other content hidden and show up the main one and have to do other task too like making the backend api request and show up the content.
+    document.querySelector(".main-content").style.display = "flex"
+    document.querySelector(".upload-content").style.display = "none"
+    homeSwitchingBtn.classList.add("active")
+    uploadSwitchingBtn.classList.remove("active")
+    localStorage.setItem("content", "main-content")
+}
+
+// Switching to Upload content
+const switchToUpload = async (event) => {
+    event.preventDefault()
+    document.querySelector(".main-content").style.display = "none"
+    document.querySelector(".upload-content").style.display = "flex"
+    uploadSwitchingBtn.classList.add("active")
+    homeSwitchingBtn.classList.remove("active")
+    localStorage.setItem("content", "upload-content")
+}
